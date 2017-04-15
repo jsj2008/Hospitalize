c0a90ebf37de47f3a1625374f9ff4783e08740b4
xingyuexing <1412690415@qq.com>
2017-04-15 13:23:51 +0800

    医院公告

---
+++ Hospitalize/HospitalInformation/HospitalNoticeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HospitalNoticeViewController.h
+//  Hospitalize
+//
+//  Created by 宋明月 on 2017/4/15.
+//  Copyright © 2017年 feichang. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface HospitalNoticeViewController : BaseViewController
+
+@end
