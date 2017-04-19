0f4fe6f3da55819ce4415655593b853a6d1e8542
xingyuexing <1412690415@qq.com>
2017-04-19 16:52:46 +0800

    文件添加

---
+++ Hospitalize/SeeDoctor/Cell/SeeDoctorTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  SeeDoctorTableViewCell.h
+//  Hospitalize
+//
+//  Created by 宋明月 on 2017/4/19.
+//  Copyright © 2017年 feichang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SeeDoctorTableViewCell : UITableViewCell
+
+@end
