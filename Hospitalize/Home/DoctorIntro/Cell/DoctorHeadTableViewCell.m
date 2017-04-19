47d339f9813c168e34585e49b0bf2571c6bf58cf
chris958 <chris958@126.com>
2017-04-19 16:26:14 +0800

    commit

---
+++ Hospitalize/Home/DoctorIntro/Cell/DoctorHeadTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  DoctorHeadTableViewCell.m
+//  Hospitalize
+//
+//  Created by 周鑫 on 2017/4/19.
+//  Copyright © 2017年 feichang. All rights reserved.
+//
+
+#import "DoctorHeadTableViewCell.h"
+
+@implementation DoctorHeadTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
