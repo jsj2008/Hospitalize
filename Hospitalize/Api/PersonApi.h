a4c4155687ecd8bdb54a41b7a8b7e589853e4d68
xingyuexing <1412690415@qq.com>
2017-05-08 17:59:43 +0800

    Api

---
+++ Hospitalize/Api/PersonApi.h
@@ -0,0 +1,13 @@
+//
+//  PersonApi.h
+//  Hospitalize
+//
+//  Created by 宋明月 on 2017/5/8.
+//  Copyright © 2017年 feichang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PersonApi : NSObject
+
+@end
---
+++ Hospitalize/Api/PersonApi.m
@@ -0,0 +1,13 @@
+//
+//  PersonApi.m
+//  Hospitalize
+//
+//  Created by 宋明月 on 2017/5/8.
+//  Copyright © 2017年 feichang. All rights reserved.
+//
+
+#import "PersonApi.h"
+
+@implementation PersonApi
+
+@end
